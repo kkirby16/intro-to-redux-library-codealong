1a58bddc564de6c58223f8e3390f988bf87d0d8a
kkirby16 <kkirby16@my.whitworth.edu>
2021-08-20 18:22:37 -0700

    commit tree 12a4619fa317779ffcb26cc8778b1bb96a2683b9

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-	handleOnClick = () => {
-		this.props.store.dispatch({
-		  type: 'INCREASE_COUNT',
-		});
-	  }
+  handleOnClick = () => {
+    this.props.store.dispatch({ type: "INCREASE_COUNT" });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleOnClick}>Click</button>
+        <p>{this.props.store.getState().clicks}</p>
+      </div>
+    );
+  }
 }
 
+// const mapStateToProps = (state) => {
+//   return {
+//     clicks: state.clicks,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCount: () => dispatch({ type: "INCREASE_COUNT" }),
+//   };
+// };
+
 export default App;
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,15 +1,26 @@
 export default function counterReducer(
-	state = {
-		clicks: 0
-	},
-	action
+  state = {
+    clicks: 0,
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				clicks: state.clicks + 1
-			}
-		default:
-			return state;
-	}
+  console.log(action);
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      console.log("Current state.clicks to %s", state.clicks);
+      console.log("Updating state.clicks to %s", state.clicks + 1);
+      return {
+        clicks: state.clicks + 1,
+      };
+    default:
+      console.log("Initial state.clicks: %s", state.clicks);
+      return state;
+  }
 }
+
+//QUESTIONS:
+//how do we have the 0 number for the count of clicks showing on the page right away at page refresh without having a bad action type dispatched such as store.dispatch({type: "@@INIT"})
+//i do see the below in the console logged on page refresh so maybe the lab or redux gave an INIT type of action to get the 0 to show?
+//{type: "@@redux/INITa.o.5.z.o.5"}
+//what is the %s used for?
+//maybe a set up a screenshare to ask what else I should know about redux dev tools.
